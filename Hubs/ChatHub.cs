1bc7aae2feb91bb82b8741dc41fa1d1c66c73fb4
tangbr <brucetang6@gmail.com>
2021-03-06 02:46:32 -0500

    On branch master  Initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Twitalike.Models;
+
+namespace Twitalike.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(Message message) =>
+            await Clients.All.SendAsync("receiveMessage", message);
+    }
+}
---
+++ Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace Twitalike.Models {
+
+
+    public class AppUser : IdentityUser {
+        // one to many    AppUser  to   Message
+        public AppUser()
+        {
+            Messages = new HashSet<Message>();
+        }
+        public virtual ICollection<Message> Messages { get; set; } 
+    }
+}
